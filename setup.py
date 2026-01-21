b07b8c4f842c4fc2d3a4c53b6e422b57e3b699be
Asish Karthikeya Gogineni <asishkarthikeyagogineni@Asishs-Laptop.local>
2026-01-21 18:59:42 -0500

    Refactor: Removed all Sage branding and assets

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 # Work-around the fact that `pip install -e .` doesn't work with `pyproject.toml` files.
 from setuptools import setup
 
-setup()
+setup(
+    name="code_chatbot",
+    version="0.1.0",
+    packages=["code_chatbot", "api"],
+    install_requires=[
+        "streamlit",
+        "langchain",
+        "chromadb",
+        "networkx",
+        "tree-sitter",
+    ],
+)
